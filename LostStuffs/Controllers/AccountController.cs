453a2816ddcc83ec6fd3b97ffc870dbced241ae2
BikovIv <ivankbikov@gmail.com>
2018-03-07 17:46:11 +0200

    commit

--- LostStuffs/Controllers/AccountController.cs
+++ LostStuffs/Controllers/AccountController.cs
@@ -78,6 +78,7 @@ namespace LostStuffs.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
+
             //var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             var user = SignInManager.UserManager.Users.Where(u => u.Email == model.Email).FirstOrDefault();
             var result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: false);
